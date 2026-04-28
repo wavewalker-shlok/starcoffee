fcf854e0b748ee8aafef8139d016bd32b58f86b0
shlok <shlokearn@gmail.com>
2026-04-28 18:09:32 +0530

    Breakpoints added

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,21 +61,54 @@ function showheader (){
 window.addEventListener('scroll' , showheader);
 
 
-/*========swiperPopular---========*/ 
- const swiperPopular = new Swiper('.popular__swiper' ,{
-  loop:true,
-  grabCursor:true,
-  spaceBetween:32,
-  slidesPerView:'auto',
-  centeredSlides:true,
+/*========swiperPopular---========*/
 
-  breakpoints:{
-    1150:{
-      spaceBetween:80,
-    }
+//  const swiperPopular = new Swiper('.popular__swiper' ,{
+//   loop:true,
+//   grabCursor:true,
+//   spaceBetween:20,
+//   slidesPerView:'auto',
+//   centeredSlides:true,
+//   
+
+//   breakpoints:{
+//     1150:{
+//       spaceBetween:20,
+//     }
+//   }
+
+//  })
+const swiperPopular = new Swiper('.popular__swiper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+
+  slidesPerView: 1,
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  speed: 800,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    
+    pagination: {
+  el: '.swiper-pagination',
+  clickable: true,
+},
+  
   }
+});
 
- })
+
 
 /*=============== SHOW SCROLL UP ===================*/  
 function  scrollUp(){
@@ -88,6 +121,8 @@ function  scrollUp(){
   }
 } 
 window.addEventListener('scroll' , scrollUp);
+
+
 
 /*============ SCROLL SECTIONS ACTIVE LINK =========*/ 
 const sections = document.querySelectorAll('section[id]');
@@ -115,3 +150,30 @@ function scrollActive() {
 }
 
 window.addEventListener('scroll', scrollActive);
+
+/*============== SCROLLREVEL ANIMATION ============*/ 
+
+const sr = ScrollReveal({
+  origin:'top',
+  distance: '60px',
+  duration: 2000,
+  delay:300,
+
+})
+
+sr.reveal(`.popular__swiper , .footer__container, .footer__copy`)
+sr.reveal(`.home__shape` , {origin:'bottom'})
+sr.reveal(`.home__coffee` , {delay:1000, distance:'200px',duration:1500})
+sr.reveal(`.home__splash` , {delay:1600, scale:0, duration:1500})
+sr.reveal(`.home__bean-1, .home__bean-2` , {delay:2200, scale:0, duration:1500, rotate:{z:180}})
+sr.reveal(`.home__ice-1, .home__ice-2` , {delay:2600, scale:0, duration:1500, rotate:{z:180}})
+sr.reveal(`.home__leaf` , {delay:2800, scale:0, duration:1500, rotate:{z:90}})
+sr.reveal(`.home__title` , {delay:3500 })
+sr.reveal(`.home__data, .home__sticker` , {delay:4000 }) 
+sr.reveal(`.about__data` , {origin:'Left'}) 
+sr.reveal(`.about__images` , {origin:'right'}) 
+sr.reveal(`.about__coffee` , {deplay:1000}) 
+sr.reveal(`.about__leaf-1, .about__leaf-2` , {deplay:1400 , rotate:{z:90}}) 
+sr.reveal(`.products__card , .contact__info` , {interval:100}) 
+sr.reveal(`.contact__shape` , {delay:600 , scale:0}) 
+sr.reveal(`.contact__delivery` , {delay:1200 }) 
